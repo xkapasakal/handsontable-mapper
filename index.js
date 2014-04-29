77d5be0e6155d2c0849f6e6b76de4f64c7f6faf2
Christos Kapasakalidis <xkapasakal@gmail.com>
2014-04-29 15:23:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// module.exports = function(){
+//   return 'Chris';
+// }
+'use strict';
+
+var _ = require('lodash');
+
+var handsontableMapper = function() {
+  // console.log(_.VERSION);
+  console.log('kapas');
+}
+
+module.exports = handsontableMapper;
